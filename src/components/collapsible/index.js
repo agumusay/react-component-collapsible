1b06e95e5fbb5abb9bd89f3f4b2a28a00069237b
agumusay <a.gumusay@protonmail.com>
2020-04-02 19:52:11 +0200

    remove annoying outline

--- src/components/collapsible/index.js
+++ src/components/collapsible/index.js
@@ -8,17 +8,16 @@ class Collapsible extends React.Component {
     };
     console.log(this.state.open);
     this.handleClick = this.handleClick.bind(this);
+    
     this.color = () => {
       let colorString = "";
       switch (true) {
         case this.props.data.category === "admin":
           colorString = "orange";
           break;
-
         case this.props.data.category === "marketing":
           colorString = "green";
           break;
-
         default:
           colorString = "gray";
           break;
